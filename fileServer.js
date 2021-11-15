be720deba2d8d3928d4c7808d3428250bf78f96a
panicMonkeyxx <colebienek@gmail.com>
2021-11-14 19:40:18 -0800

    changes_11-14

--- fileServer.js
+++ fileServer.js
@@ -1,9 +1,17 @@
-const fileSystem = require('fs');
-
-
-
-function getTodos () {
-    return "Get Milk and Take Out Trash"
+const fileSystem = require ('fs');
+function getTodos(req, res) {
+    fileSystem.readFile('./fileServe.txt', 'utf-8',(err, fileData) =>{
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.set('content-type', 'text/html');
+            res.status(200).send(fileData);
+        }
+    })
 }
+
+function saveTodo(req, res) {
+    fileSystem.appendFile('./fileServe.txt', '\nwarm up the cuddles', 'utf8', getTodos)
+};
 
 module.exports = {getTodos:getTodos};
--- server.js
+++ server.js
@@ -1,22 +1,20 @@
+
 const express = require ('express');
 const app = express();
 const fileServer = require('./fileServer.js');
 const PORT_NUMBER = 3000;
-
-// console.log(fileServer.getTodos());
-
-
-app.get('/', function(req,res){
-    res.send('Hello Whole Wide World!!')
-})
+const axios = require('axios').default;
 
 function generateConnectionMessage(errorMsg) {
-   let displayMsg = errorMsg ? errorMsg : "conected to port: " + PORT_NUMBER;
-   console.log(displayMsg);
-}
+    let displayMsg = errorMsg ? errorMsg : "conected to port: " + PORT_NUMBER;
+    console.log(displayMsg);
+ }
+
 //serve static files, i.e. images, CSS, JS==>express.static(root, [options])
 app.use(express.static('public'));
 
+//the public folder & file with functions
+app.get('/todos', fileServer.getTodos)
 
 //activate the listener
 app.listen(PORT_NUMBER, generateConnectionMessage);
