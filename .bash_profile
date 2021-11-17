6e9743921e76b9c217b80f0ff92dc4ad89a5597d
cole <colebienek@gmail.com>
2021-11-17 14:59:35 -0800

    mongo_mongoosePlay

---
+++ .bash_profile
@@ -0,0 +1,1 @@
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin:/Applications/mongodb-macos-x86_64-5.0.4/bin
--- server.js
+++ server.js
@@ -3,17 +3,40 @@ const express = require ('express');
 const app = express();
 // const fileServer = require('./fileServer.js');
 const PORT_NUMBER = 3000;
-
+const mongoose = require('mongoose');
+const mongoClient = require('mongodb').mongoClient;
+const mongoURL = 'mongodb://localhost:27017/';
 
 function generateConnectionMessage(errorMsg) {
     let displayMsg = errorMsg ? errorMsg : "conected to port: http//:localhost " + PORT_NUMBER;
     console.log(displayMsg);
  }
- app.get('/', (req, res)=>{
-    res.send("Welcome to your server!")
+
+// main().catch(err => console.log(err));
+
+// async function main(){
+//     await mongoose.connect('mongodb://localhost:27017/test');
+//     const kittySchema = new mongoose.Schema({
+//         name: String
+//     });
+//     const kitten = mongoose.model('Kitten', kittySchema);
+//     const silence = new kitten({name: 'Silence'});
+//     console.log(silence.name); 
+// }
+
+app.get('/:userName', (req, res)=>{
+    res.send('Your name is ' + req.params.userName + '\n')
 })
 //serve static files, i.e. images, CSS, JS==>express.static(root, [options])
-app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    res.json(req.body);
+})
+
+
+// app.use(express.static('public'));
+
 
 
 
